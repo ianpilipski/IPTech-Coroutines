e6b18b4afda352f5ba3dad5170e70fe0840557b3
Ian Pilipski <ian@pilipski.com>
2019-07-08 17:36:15 -0400

    adding travis build support

---
+++ travis-build/run-tests.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+PROJECT_PATH=$(pwd)/$UNITY_PROJECT_PATH
+UNITY_BUILD_DIR=$(pwd)/Build
+LOG_FILE=$UNITY_BUILD_DIR/unity-test.log
+
+
+ERROR_CODE=0
+echo "Running unity editor tests..."
+mkdir $UNITY_BUILD_DIR
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile \
+  -projectPath "$PROJECT_PATH" \
+  -runEditorTests
+  | tee "$LOG_FILE"
+  
+if [ $? = 0 ] ; then
+  echo "Tests completed successfully."
+  ERROR_CODE=0
+else
+  echo "Tests failed. Exited with $?."
+  ERROR_CODE=1
+fi
+
+echo "Finishing with code $ERROR_CODE"
+exit $ERROR_CODE
