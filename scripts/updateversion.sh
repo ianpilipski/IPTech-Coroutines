a9cb4fe497c75133a08b089e4facc983538441b9
Ian Pilipski <ian@pilipski.com>
2021-03-08 00:18:19 -0500

    update versions script updated and documentation copied

--- scripts/updateversion.sh
+++ scripts/updateversion.sh
@@ -21,14 +21,20 @@ function update_project_settings() {
 
 function update_package_settings() {
   echo Updating version in IPTech.Coroutines package
-  sed -i '' "s/\"version\": \"1\.1\.1\"/\"version\": \"$VERSION\"/" ./IPTech-Coroutines/Assets/IPTech.Coroutines/package.json
+  sed -i '' "s/\"version\": \"[0-9]*\.[0-9]*\.[0-9]*\"/\"version\": \"$VERSION\"/" ./IPTech-Coroutines/Packages/IPTech.Coroutines/package.json
+}
 
-  echo Updating version in IPTech.Coroutines.Examples package
-  sed -i '' "s/\"version\": \"1\.1\.1\"/\"version\": \"$VERSION\"/" ./IPTech-Coroutines/Assets/IPTech.Coroutines.Examples/package.json
+function update_readme_settings() {
+  echo Updating version in IPTech.Coroutines README
+  sed -i '' "s/\"com.iptech.coroutines\": \"[0-9]*\.[0-9]*\.[0-9]*\"/\"com.iptech.coroutines\": \"$VERSION\"/" ./IPTech-Coroutines/Packages/IPTech.Coroutines/README.md
+
+  cp ./IPTech-Coroutines/Packages/IPTech.Coroutines/README.md ./IPTech-Coroutines/Packages/IPTech.Coroutines/Documentation~/README.md
+  cp ./IPTech-Coroutines/Packages/IPTech.Coroutines/README.md README.md
 }
 
 check_for_valid_arguments
 update_project_settings
 update_package_settings
+update_readme_settings
 
 echo Version Updated
